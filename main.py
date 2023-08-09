51a666d0925326d3b7cff2d287d3457368a534ee
FusiDaniel <fpires.daniel@gmail.com>
2023-08-09 06:51:27 -0300

    first working

--- main.py
+++ main.py
@@ -2,19 +2,16 @@
 import numpy as np
 import random
 import time
-
-'''
-0 = vazio
-1 = atencao
-2 = morte
-3 = brilho
-4 = ouro
-5 = entrada/saida
-'''
-
+import neat
 import os
-def clear():
-    os.system('cls')
+import pickle
+import visualize
+
+ # feed_forward            = True change to False with you want a recur
+ # 0 a 6 + pegou o ouro
+
+# 0 = vazio, 1 = atencao, 2 = morte, 3 = brilho, 4 = ouro, 5 = entrada/saida
+
 
 baseMap = np.array([
     [0,1,0,1,2,1,0,0,1,0],
@@ -34,6 +31,7 @@ reverse_dir_dic = {(-1, 0): 'c', (0, 1): 'd', (1, 0): 'b', (0, -1): 'e'}
 person_dict = {'c' : '⬆️', 'b' : '⬇️', 'd' : '➡️', 'e' : '⬅️'}
 char_vector = ['⬜','🌀','😈','✨','🏆','🏁','⬛','❓']
 mapped_movements = {11: 'l', 12: 'r', 13: 'b', 3: 'f', 0: 'g', 1: 'v'}
+mapped_movements_2 = ['l','r','b','f','g','v']
 
 def print_state(map, pos, direction, grabbed):
   for i in range(map.shape[0]):
@@ -48,6 +46,12 @@ def print_state(map, pos, direction, grabbed):
   if grabbed:
     print(char_vector[4], end='')
 
+def check_death(map, pos):
+  if map[pos] == 2:
+    clear()
+    return True
+  return False
+
 def sense(map, pos, dir):
   shape = map.shape
   dir = dir_dic[dir]
@@ -56,6 +60,13 @@ def sense(map, pos, dir):
   if dest[0] < 0 or dest[1] < 0 or shape[0] <= dest[0] or shape[1] <= dest[1]:
     return 6
   return map[dest]
+
+def senseVector( map, pos, dir):
+  vector = np.zeros((3, 7), dtype=np.int32)
+  vector[0][sense(map, pos, dir)] = 1
+  vector[1][sense(map, pos, directions[(directions.index(dir) - 1) % 4])] = 1
+  vector[2][sense(map, pos, directions[(directions.index(dir) + 1) % 4])] = 1
+  return vector
 
 def move(map, pos, dir, command, grabbed, win):
   if command == 'r':
@@ -78,105 +89,98 @@ def move(map, pos, dir, command, grabbed, win):
     if map[pos] == 5 and grabbed:
       win = True
   return pos, dir, grabbed, win
-def check_death(map, pos):
-  if map[pos] == 2:
-    clear()
-    return True
-  return False
-
-def make_decision(probabilities):
-    items_list = list(probabilities.keys())
-    probabilities_list = list(probabilities.values())
-
-    return random.choices(items_list, weights=probabilities_list, k=1)[0]
-
-def hasFlash(vecInpSens: np.int32, i: int) -> bool:
-    return vecInpSens[i][3] == 1
-def hasGoal(vecInpSens: np.int32, i: int) -> bool:
-    return vecInpSens[i][4] == 1
-def hasObstacle(vecInpSens: np.int32, i: int) -> bool:
-    return vecInpSens[i][2] == 1 or vecInpSens[i][6] == 1
-
-last_choice = -1
-reached_goal = False
 
 def infer(vecInpSens: np.int32) -> int:
-    global last_choice, memory, reached_goal
-    outy = -1  # por default, o índice de saída é um índice de erro
+    return 3     
+
+# if __name__ == '__main__':
+#   map = np.array(baseMap, copy=True)
+#   pos = (3,3)
+#   energy = 300
+#   dir = random.choice(directions)
+#   grabbed = False
+#   win = False
+#   i = 0
+#   while energy > 0:
+#     os.system('cls')
+#     print('Energia: ', energy)
+#     print_state(map,pos,dir, grabbed)
+#     # command = input('l, r, b, f, g, v: ')
+#     vector = senseVector(map, pos, dir)
     
-    if np.sum(vecInpSens) == 0 :  # se num_input_bits for zero
-        return outy  # retorna erro (-1)
-    else:
-        if (reached_goal):
-            outy = 0
-        elif (hasFlash(vecInpSens, 0)):
-            outy = 3
-        elif (hasFlash(vecInpSens, 1) or hasGoal(vecInpSens, 1)):
-            outy = 11
-        elif (hasFlash(vecInpSens, 2) or hasGoal(vecInpSens, 2)):
-            outy = 12
-        elif (hasGoal(vecInpSens, 0)):
-            outy = 3
-            reached_goal = True
+#     command = mapped_movements[infer(vector)]
+#     pos, dir, grabbed, win = move(map, pos, dir, command, grabbed, win)
+#     energy -= 1
+#     if map[pos] == 2:
+#       os.system('cls')
+#       print('Energia: ', energy)
+#       print_state(map,pos,dir, grabbed)
+#       print('Morreu')
+#       break
+#     if win or grabbed:
+#       print('Venceu')
+#       break
+#     i += 1
+#     # time.sleep(0.01)
 
-        elif (hasObstacle(vecInpSens, 0)):
-            if (hasObstacle(vecInpSens, 1)):
-                outy = make_decision({12: 6, 13: 1})
-            elif (hasObstacle(vecInpSens, 2)):
-                outy = make_decision({11: 6, 13: 1})
-                    
-            else:
-                if (last_choice == 11) :
-                    outy = make_decision({11: 8, 13: 1})
-                if (last_choice == 12) :
-                    outy = make_decision({11: 8, 13: 1})
-                else:
-                    outy = make_decision({11: 4, 12: 4, 13: 1})
-        else:
-            if (last_choice == 11 or last_choice == 12):
-                outy = 3
-            elif (hasObstacle(vecInpSens, 1)):
-                outy = make_decision({3: 7, 12: 1})
-            elif (hasObstacle(vecInpSens, 2)):
-                outy = make_decision({3: 7, 11: 1})
-            else:
-                outy = make_decision({3: 7, 11: 1, 12: 1})
-    last_choice = outy
-    return outy
-
-
-def senseVector( map, pos, dir):
-  vector = np.zeros((3, 13), dtype=np.int32)
-  vector[0][sense(map, pos, dir)] = 1
-  vector[1][sense(map, pos, directions[(directions.index(dir) - 1) % 4])] = 1
-  vector[2][sense(map, pos, directions[(directions.index(dir) + 1) % 4])] = 1
-  return vector
-
-map = np.array(baseMap, copy=True)
-pos = (3,3)
-energy = 300
-dir = random.choice(directions)
-grabbed = False
-win = False
-i = 0
-while energy > 0:
-  clear()
-  print('Energia: ', energy)
-  print_state(map,pos,dir, grabbed)
-  # command = input('l, r, b, f, g, v: ')
-  vector = senseVector(map, pos, dir)
-  
-  command = mapped_movements[infer(vector)]
-  pos, dir, grabbed, win = move(map, pos, dir, command, grabbed, win)
-  energy -= 1
-  if map[pos] == 2:
-    clear()
+def train_ai(genome, config):
+  net = neat.nn.FeedForwardNetwork.create(genome, config)
+  map = np.array(baseMap, copy=True)
+  pos = (3,3)
+  energy = 500
+  dir = random.choice(directions)
+  grabbed = False
+  win = False
+  i = 0
+  while energy > 0:
+    energy -= 3
+    vector = np.append(senseVector(map, pos, dir).reshape(1, 21), grabbed).reshape(1, 22)
+    output = net.activate(vector[0])
+    command = mapped_movements_2[output.index(max(output))]
+    pos, dir, grabbed, win = move(map, pos, dir, command, grabbed, win)
+    os.system('cls')
+    print(output)
+    print(command)
     print('Energia: ', energy)
     print_state(map,pos,dir, grabbed)
-    print('Morreu')
-    break
-  if win or grabbed:
-    print('Venceu')
-    break
-  i += 1
-  # time.sleep(0.01)
+    if map[pos] == 2:
+      break
+    if win or grabbed:
+      break
+    i += 1
+  genome.fitness = i
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+      genome.fitness = 500
+      train_ai(genome, config)
+
+
+def run_neat(config):
+    #p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-85')
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(1))
+
+    winner = p.run(eval_genomes, 1) # run for 50 generations
+    with open("best.pickle", "wb") as f:
+        pickle.dump(winner, f)
+    print('\nBest genome:\n{!s}'.format(winner))
+
+    # Show output of the most fit genome against training data.
+    print('\nOutput:')
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    run_neat(config)
+    # test_best_network(config)
